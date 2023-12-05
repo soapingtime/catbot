cc9d19cba89a42c142992ecf6b797025abc7bc63
soap <twinkd3ath@proton.me>
2023-12-05 13:03:09 -0600

    typo

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# get urls from twiiit.com. they do most of the work for filtering out ratelimited/dead instances
+
+uni_url=$(curl https://twiiit.com/unicouniuni3 -s -L -I -o /dev/null -w '%{url_effective}')
+alfie_url=$(curl https://twiiit.com/goodboyalfie -s -L -I -o /dev/null -w '%{url_effective}')
+
+# save images for 2 minutes, then automatically cancel. prefix with nitter: to use an instance that isn't in the gallery-dl list
+timeout 120s gallery-dl "nitter:$uni_url"
+# less time because it's an inactive account
+timeout 45s gallery-dl "nitter:$alfie_url"
+
+mv gallery-dl/*/unicouniuni3/* uni/
+mv gallery-dl/*/goodboyalfie/* alfie/
+
+rm -rf gallery-dl
+
+git pull
+git commit -am "[automated] update images"
+git push -u
